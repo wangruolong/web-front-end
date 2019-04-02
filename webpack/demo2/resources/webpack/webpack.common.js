cd0ee9ffe0e4cfe67ef143f3fb7a296db5824f22
王若龙 <150423@nd>
2019-04-02 11:54:18 +0800

    完善webpack文档

--- webpack/demo2/resources/webpack/webpack.common.js
+++ webpack/demo2/resources/webpack/webpack.common.js
@@ -5,6 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
 console.info('----------------------------------------------------------------------------------------------------')
 console.info('从这里可以看出当前执行的路径。说明webpack里面的路径是参照package.json所在的位置的。')
 console.info('process.cwd()是当前进程的工作目录',process.cwd())
@@ -12,20 +13,20 @@ console.info('__dirname是当前js文件所处的路径',__dirname)
 console.info('----------------------------------------------------------------------------------------------------')
 
 module.exports = {
-    entry: {//入口
+    entry: {// 入口
         app: './src/index.js',
         //在这里打包一些和app.js重复的包，然后再通过optimization.splitChunks把这些公共的包抽出来放到common.js里面。用这种方式来达到剥离app.js里面大文件的目的，使得app.js的文件变小。
         //venders一般是配合optimization.splitChunks使用。
         venders: ['jquery']
     },
-    output: {//出口
+    output: {// 出口
         filename: '[name].[chunkhash].js',
         //__dirname是path中的变量，是当前js文件所处的路径。**/demo4/resources/webpack/
         //process.cwd()返回当前进程的工作目录。就是返回package.json的目录。**/demo4/
         path: path.join(process.cwd(), 'dist')
     },
     plugins: [
-        //第一个参数是个数组，可以指定清除多个路径的文件。第二个参数是对CleanWebpackPlugin的配置。
+        // 第一个参数是个数组，可以指定清除多个路径的文件。第二个参数是对CleanWebpackPlugin的配置。
         new CleanWebpackPlugin([path.join(process.cwd(), 'dist')], {
             allowExternal: true,// 该插件默认只能清除当前配置所处的文件夹，开启allowExternal=true使其可以清除当前目录之外的文件夹。
             exclude: ['WEB-INF']// 一般web应用中都会有WEB-INF文件夹里面有web.xml，可以把这个文件夹排除掉不清除。
@@ -34,17 +35,17 @@ module.exports = {
             title: 'tetris',
             template: path.join(__dirname, '../template/index-template.html')
         }),
-        //可以在代码中直接使用 _  $ jion
+        // 可以在代码中直接使用 _  $ jion
         new webpack.ProvidePlugin({
             _: 'lodash',
             $: 'jquery',
             join: ['lodash', 'join']
         }),
-        //抽取css。把css从js单独剥离出来成为另外一个文件。
+        // 抽取css。把css从js单独剥离出来成为另外一个文件。
         new MiniCssExtractPlugin({
             filename: "[name].[contenthash].css"
         }),
-        //压缩css。把格式化的有换行有tab的css，压缩成只有一行的css。
+        // 压缩css。把格式化的有换行有tab的css，压缩成只有一行的css。
         new OptimizeCSSAssetsPlugin(),
         // 包分析器，可以分析打包后每个文件的大小。
         new BundleAnalyzerPlugin()
@@ -66,20 +67,25 @@ module.exports = {
             {
                 test: /\.(scss|css)$/,
                 use: [
-                    //把css文件打包成<link></link>的形式诸如到页面中的<head></head>里面。把css文件从js充剥离成单独的文件，要在这里加入Loader，和上面的MiniCssExtractPlugin配套使用才会生效。
-                    MiniCssExtractPlugin.loader,
-                    // <style></style>和<link></link>的区别是前者跟着页面一起返回，后者需要再请求一次。
-                    // 由此可见如果页面文件比较大可以用link的方式进行分次请求就不用一次请求很久才回来，如果页面文件比较小可以用style和页面一起返回就只要请求一次就可以了不用请求多次。
+                    // MiniCssExtractPlugin.loader和style-loader二选一，他们的区别是。
+                    // 相同之处：
+                    // 1.他们都是把css-loader加载后的样式文件独立出来。
+                    // 不同之处：
+                    // 1.独立方式不一样。MiniCssExtractPlugin.loader，把css文件打包成<link />的形式诸如到页面中的<head />里面；style-loader把css文件打包成以<style />的形式注入到页面中<head />里面。
+                    // 2.返回时间点不一样。<link />和<style />的区别是前者需要再请求一次，后者跟着页面一起返回不用再请求一次。
+                    // 3.应用场景不一样。如果页面文件比较大可以用<link />的方式进行分次请求就不用一次请求很久才回来，如果页面文件比较小可以用style和页面一起返回就只要请求一次就可以了不用请求多次。
+                    MiniCssExtractPlugin.loader,//把css文件打包成<link></link>的形式诸如到页面中的<head></head>里面。把css文件从js充剥离成单独的文件，要在这里加入Loader，和上面的MiniCssExtractPlugin配套使用才会生效。
                     // 'style-loader',//把css文件打包成以<style></style>的形式注入到页面中<head></head>里面。
-                    // css-loader处理用import *.css语法导入的文件
                     {
-						loader: 'css-loader',
+						loader: 'css-loader',// css-loader用来处理import Styles from 'xxx.css'语法导入的文件。
 						options: {
-							modules: true, // 开启modules=true使得用户可以通过import styles from 'abc.css'，使得可以使用styles['key']获取样式表里面的样式
+							modules: true, // 开启modules=true使得用户可以通过import styles from 'abc.css'，使得可以使用styles['key']获取样式表里面的样式。
 							localIdentName: '[path][name]__[local]--[hash:base64:5]' // 为了防止样式名称重复，可以开启localIdentName，它会对css的名称进行干扰，保证全局唯一。
 						}
                     },
-                    { loader: 'sass-loader' }// sass-loader依赖两个包： "sass-loader": "^7.1.0"和 "node-sass": "^4.11.0"，后者是用来让sass运行在node环境中。
+                    { 
+                        loader: 'sass-loader' // sass-loader依赖两个包： "sass-loader": "^7.1.0"和 "node-sass": "^4.11.0"，后者是用来让sass运行在node环境中。
+                    }
                 ]
             },
             {
@@ -88,7 +94,7 @@ module.exports = {
                     {
                         loader:'file-loader',
                         options: {
-                            outputPath:'assets/'//把输打包的资源文件放到assets里面
+                            outputPath:'assets/'// 把打包的资源文件放到assets里面
                         }
 				    }
                 ]
