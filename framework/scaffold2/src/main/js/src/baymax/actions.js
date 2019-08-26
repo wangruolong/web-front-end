bd76f956331f6fb61077b20baf367a63c33599ec
wangruolong <wangruolong@msn.cn>
2019-08-26 18:59:37 +0800

    修改代码

--- framework/scaffold2/src/main/js/src/baymax/actions.js
+++ framework/scaffold2/src/main/js/src/baymax/actions.js
@@ -7,71 +7,71 @@ const effects = {}
 
 // 把reducers解析成action和pureFunctions
 function resolveReducers(modelName, reducers = {}, effects = {}) {
-    // 先初始化modelName
-    actions[modelName] = {}
+	// 先初始化modelName
+	actions[modelName] = {}
 
-    // 解析reducers里面的key => action 
-    Object.keys(reducers).forEach(actionName => {
-        actions[modelName][actionName] = (data)=>(
-            dispatch({
-                type: `${modelName}${SEP}${actionName}`,
-                data
-            })
-        )
-    })
-    // 解析reducers里面的value => pure functions
-    pureFunctions[modelName] =  Object.keys(reducers).reduce((acc, cur) => {
-        acc[`${modelName}${SEP}${cur}`] = reducers[cur]
-        return acc
-    }, {})
+	// 解析reducers里面的key => action 
+	Object.keys(reducers).forEach(actionName => {
+		actions[modelName][actionName] = (data)=>(
+			dispatch({
+				type: `${modelName}${SEP}${actionName}`,
+				data
+			})
+		)
+	})
+	// 解析reducers里面的value => pure functions
+	pureFunctions[modelName] =  Object.keys(reducers).reduce((acc, cur) => {
+		acc[`${modelName}${SEP}${cur}`] = reducers[cur]
+		return acc
+	}, {})
 
 }
 let data = {
-    name: 'todos',
-    initialState: {
-      list: [],
-      visibility: 'all'
-    },
-    reducers: {
-      add(state, text) {
-        return {
-          ...state,
-          list: [
-            ...state.list,
-            {id: nextId++, text}
-          ]
-        }
-      },
-      toggle(state, id) {
-        return {
-          ...state,
-          list: state.list.map(d => {
-            if (d.id === id) {
-              d.completed = !d.completed
-            }
-            return d
-          })
-        }
-      },
-      setVisibility(state, filter) {
-        return {
-          ...state,
-          visibility: filter
-        }
-      }
-    },
-    effects:{
-      addTodo(){
+	name: 'todos',
+	initialState: {
+		list: [],
+		visibility: 'all'
+	},
+	reducers: {
+		add(state, text) {
+			return {
+				...state,
+				list: [
+					...state.list,
+					{id: nextId++, text}
+				]
+			}
+		},
+		toggle(state, id) {
+			return {
+				...state,
+				list: state.list.map(d => {
+					if (d.id === id) {
+						d.completed = !d.completed
+					}
+					return d
+				})
+			}
+		},
+		setVisibility(state, filter) {
+			return {
+				...state,
+				visibility: filter
+			}
+		}
+	},
+	effects:{
+		addTodo(){
         
-      }
-    }
-  }
+		}
+	}
+}
 
 resolveReducers(data.name,data.reducers)
 
-var _redux = require("redux");
+var _redux = require('redux')
 const rootReducer = _redux.combineReducers({
-  pureFunctions
+	pureFunctions
 })
 
 console.log('actions',actions)
--- framework/scaffold2/src/main/js/src/baymax/index.js
+++ framework/scaffold2/src/main/js/src/baymax/index.js
@@ -1,22 +1,22 @@
 import { getReducer, addActions } from './actions'
 
 model({
-    name: 'todos',
-    initialState: {
-      list: [],
-      visibility: 'all'
-    },
-    reducers: {
-      add(state, text) {
-        return {
-          ...state,
-          list: [
-            ...state.list,
-            {id: nextId++, text}
-          ]
-        }
-      }
-    }
+	name: 'todos',
+	initialState: {
+		list: [],
+		visibility: 'all'
+	},
+	reducers: {
+		add(state, text) {
+			return {
+				...state,
+				list: [
+					...state.list,
+					{id: nextId++, text}
+				]
+			}
+		}
+	}
 })
 
 // 全局models
@@ -24,28 +24,28 @@ export const models = []
 
 
 export default function model(m) {
-  const reducer = getReducer(resolveReducers(m.name, m.reducers), m.initialState)
-  const _model = {
-    name: m.name,
-    reducer
-  }
-  models.push(_model)
-  addActions(m.name, m.reducers, m.effects)
-  return _model
+	const reducer = getReducer(resolveReducers(m.name, m.reducers), m.initialState)
+	const _model = {
+		name: m.name,
+		reducer
+	}
+	models.push(_model)
+	addActions(m.name, m.reducers, m.effects)
+	return _model
 }
 
 export const baymaxReducer={}
 
 function createReducer(models, reducers) {
 
-    const modelReducers = models.reduce((acc, cur) => {
-      acc[cur.name] = cur.reducer
-      return acc
-    }, {})
+	const modelReducers = models.reduce((acc, cur) => {
+		acc[cur.name] = cur.reducer
+		return acc
+	}, {})
   
-    return combineReducers({
-      ...modelReducers
-    })
+	return combineReducers({
+		...modelReducers
+	})
   
-  }
+}
   
